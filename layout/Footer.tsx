51174abc8ab7958902dece3e3bbb862f3f4f5f71
alexanderota119 <alexanderota119@gmail.com>
2022-12-06 04:32:07 +0900

    fix: update footer part;add the link to certificated img

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import Script from 'next/script'
 import Image from 'next/image'
 import Link from 'next/link'
 import Container from '@/components/container'
@@ -7,7 +8,10 @@ const Footer: FC = () => {
   return (
     <footer className="mt-36 bg-[rgb(232,232,232)] py-5">
       <Container className="flex flex-col items-center gap-20">
-        <Image src="/assets/images/certified.png" alt="no img" width={140} height={120} />
+        <Script src="https://static.legitscript.com/seals/11058625.js"></Script>
+        <Link href="https://legitscript.com" target={'_blank'} title="Verify LegitScript Approval">
+          <Image src="/assets/images/certified.png" alt="no img" width={140} height={120} />
+        </Link>
         <p className="text-center text-[#37465A] text-[0.8669rem] leading-[1.375rem]">
           MD Exam Copyright © 2022. All rights reserved.
           <br />
